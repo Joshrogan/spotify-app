9649706875708cd0a3d9be8a5680fc1925f92d16
Josh Rogan <joshrogan93@gmail.com>
2023-08-03 22:11:15 +0100

    retry with config updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/spotify-app/",
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  },
 });
