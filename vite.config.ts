c7b5da402fff6e27b72e957a5b08b384032497f9
Josh Rogan <joshrogan93@gmail.com>
2023-08-03 22:27:51 +0100

    revereted vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/spotify-app/",
-//   plugins: [react()],
-// });
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv("mock", process.cwd(), "");
-  const processEnvValues = {
-    "process.env": Object.entries(env).reduce((prev, [key, val]) => {
-      console.log(key, val);
-      return {
-        ...prev,
-        [key]: val,
-      };
-    }, {}),
-  };
-
-  return {
-    plugins: [react()],
-    define: processEnvValues,
-  };
+export default defineConfig({
+  base: "/spotify-app/",
+  plugins: [react()],
 });
