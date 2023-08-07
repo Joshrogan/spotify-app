0dca02e3992145f049f4115a6202dfaace9f7ed1
Josh Rogan <joshrogan93@gmail.com>
2023-08-07 16:49:29 +0100

    prod devtools

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,8 +1,16 @@
+import React from "react";
 import { Outlet, RootRoute } from "@tanstack/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "../router";
 import NavBar from "../components/navbar/NavBar";
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
 
 interface MyRouterContext {
   queryClient: QueryClient;
@@ -16,7 +24,10 @@ const rootRoute = RootRoute.withRouterContext<MyRouterContext>()({
           <NavBar />
           <Outlet />
         </div>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-left" />
+        <ReactQueryDevtoolsProduction
+          initialIsOpen={false}
+          position="bottom-left"
+        />
       </QueryClientProvider>
     );
   },
